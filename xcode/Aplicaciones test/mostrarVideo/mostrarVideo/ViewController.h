8f5c00e71ffb2775c9ed105b70b2ccbb75824a22
encuadro <encuadro@mini.(none)>
2012-09-14 20:14:10 -0300

    agrego app que muestra video, actualizo caso uso

---
+++ xcode/Aplicaciones test/mostrarVideo/mostrarVideo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  mostrarVideo
+//
+//  Created by encuadro augmented reality on 9/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
