f28454d74b35beaf16fc9b32995e78d72b86edcc
encuadro <encuadro@mini.(none)>
2012-09-15 19:13:27 -0300

    Creo caso de uso 2, proyeccion de video

---
+++ xcode/casosUso/casoUso0200/casoUso0200/Composit.h
@@ -0,0 +1,14 @@
+//
+//  Composit.h
+//  CoplanarPosit
+//
+//  Created by Juan Ignacio Braun on 3/16/12.
+//  Copyright (c) 2012 juanibraun@gmail.com. All rights reserved.
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+void Composit(long int np,double** coplImage,double** copl, double fLength, double** R, double* T);
