4aaf95fe22e0a32510ebb7640ee69710be210646
encuadro <encuadro@mini.(none)>
2012-09-18 00:10:44 -0300

    creo demo07 que incluye opcion camara y opcion video de archivo para procesar. Falta acceder a pixeles

---
+++ xcode/Demo/demo07/demo07/main.m
@@ -0,0 +1,17 @@
+//
+//  demo07main.h
+//  demo07
+//
+//  Created by encuadro augmented reality on 9/17/12.
+//  Copyright 2012 __MyCompanyName__. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	UIApplicationMain(argc, argv, nil, @"demo07AppDelegate");
+	[pool release];
+	return 0;
+}
