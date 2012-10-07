49413316d6dbe381be5f82ee5c4387277e7ed4c3
encuadro <encuadro@mini.(none)>
2012-10-07 18:45:36 -0200

    tuneando el caso uso 02, v2

--- xcode/casosUso/casoUso0200/casoUso0200/Isgl3dViewController.m
+++ xcode/casosUso/casoUso0200/casoUso0200/Isgl3dViewController.m
@@ -245,8 +245,8 @@ double* luminancia;
         
         for (int i=0; i<4; i++) {
             
-            imagePoints4[i][0]=imagePoints[i+8][0]*wSize/480;
-            imagePoints4[i][1]=imagePoints[i+8][1]*hSize/360;
+            imagePoints4[i][0]=imagePoints[i+4][0]*wSize/480;
+            imagePoints4[i][1]=imagePoints[i+4][1]*hSize/360;
             
 //            imagePoints4[i][0]=(imagePoints[i+4][0]-imagePoints[6][0])*wSize/480;
 //            imagePoints4[i][1]=(imagePoints[i+4][1]-imagePoints[6][1])*hSize/360;
@@ -418,7 +418,7 @@ double* luminancia;
     NSURL *movieURL = [NSURL fileURLWithPath:moviePath];
     theMovie = [[MPMoviePlayerController alloc] initWithContentURL:movieURL];
     //Place it in subview, else it won’t work
-    theMovie.view.frame = CGRectMake(0, 0, 90, 90);
+    theMovie.view.frame = CGRectMake(0, 0, 60, 60);
     //theMovie.fullscreen=YES;
     theMovie.controlStyle=MPMovieControlStyleNone;
     //theMovie.view.contentMode=UIViewContentModeScaleToFill;
@@ -464,17 +464,17 @@ double* luminancia;
     imagePoints3=(double **)malloc(4 * sizeof(double *));
     for (i=0;i<4;i++) imagePoints3[i]=(double *)malloc(2 * sizeof(double));
     
-    imagePoints3[0][0]=90;
-    imagePoints3[0][1]=90;
+    imagePoints3[0][0]=60;
+    imagePoints3[0][1]=60;
     
-    imagePoints3[1][0]=90;
+    imagePoints3[1][0]=60;
     imagePoints3[1][1]=0;
     
     imagePoints3[2][0]=0;
     imagePoints3[2][1]=0;
     
     imagePoints3[3][0]=0;
-    imagePoints3[3][1]=90;
+    imagePoints3[3][1]=60;
     
     
     //imagePoints4 guarda los puntos detectados con el ajuste de pantalla
