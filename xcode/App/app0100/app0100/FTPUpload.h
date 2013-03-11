3deeb3f5d1995a0d937e04f3d11e2cd855e8e0a5
encuadro <c.fleitas@gmail.com>
2013-03-11 15:24:10 -0300

    Upload

---
+++ xcode/App/app0100/app0100/FTPUpload.h
@@ -0,0 +1,13 @@
+//
+//  FTPUpload.h
+//  app0100
+//
+//  Created by encuadro on 3/8/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FTPUpload : NSObject
+
+@end
---
+++ xcode/App/app0100/app0100/FTPUpload.m
@@ -0,0 +1,13 @@
+//
+//  FTPUpload.m
+//  app0100
+//
+//  Created by encuadro on 3/8/13.
+//
+//
+
+#import "FTPUpload.h"
+
+@implementation FTPUpload
+
+@end
